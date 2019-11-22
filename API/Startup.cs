9ecad117de876defe5f34e51cb5821ad7a50d15b
Saeed Alsharqawi <saed_sharqawi@hotmail.com>
2019-11-22 23:17:04 +0300

    Adding Semantic UI, Adding Cors support to API Project

--- API/Startup.cs
+++ API/Startup.cs
@@ -20,12 +20,21 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(opt => {
+            services.AddDbContext<DataContext>(opt =>
+            {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
 
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
+
             services.AddControllers();
-        
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,7 +46,7 @@ namespace API
             }
 
             //app.UseHttpsRedirection();
-
+            app.UseCors("CorsPolicy");
             app.UseRouting();
 
             app.UseAuthorization();
